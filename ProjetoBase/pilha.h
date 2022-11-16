53c6a4d0a21772a4512a3f7bd144fa7821ca95fa
Arthur Martinelli <ama18@inf.ufpr.br>
2022-11-16 03:13:34 -0300

    pilha feita e testada, só n sei se completa

---
+++ ProjetoBase/pilha.h
@@ -0,0 +1,19 @@
+#ifndef __STACK__
+#define __STACK__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack_t {
+    void **p;
+    int tam;
+    int max_tam;
+} stack_t;
+
+stack_t *init_pilha();
+int push(stack_t *s, void *elem);
+void *pop(stack_t *s);
+void print_pilha(stack_t *s, void print_elem (void*));
+void *busca(stack_t *s, int igual(void *, void *), void *elem);
+
+#endif
